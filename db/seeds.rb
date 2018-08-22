7967aa2314c7b56a6ead8417580e7e2600a6d580
Andrew Och <andrewoch95@gmail.com>
2018-08-22 18:53:37 -0400

    A few more feature tweaks.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
-user1 = User.create(name: "Andrew Och", email: "andrew@gmail.com", password_digest: "andrew")
-user2 = User.create(name: "Ann Kim", email: "ann@gmail.com", password_digest: "ann")
-user3 = User.create(name: "Ali Och", email: "ali@gmail.com", password_digest: "ali")
-
-tweet1 = Tweet.create(content: "Hello World!", user: user1)
-
-comment1 = Comment.create(content: "Hello", tweet: tweet1, user: user1)
+# 
+# user1 = User.create(name: "Andrew Och", email: "andrew@gmail.com", password_digest: "andrew")
+# user2 = User.create(name: "Ann Kim", email: "ann@gmail.com", password_digest: "ann")
+# user3 = User.create(name: "Ali Och", email: "ali@gmail.com", password_digest: "ali")
+#
+# tweet1 = Tweet.create(content: "Hello World!", user: user1)
+#
+# comment1 = Comment.create(content: "Hello", tweet: tweet1, user: user1)
